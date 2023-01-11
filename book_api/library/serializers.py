2c2fb747802e93d5dc4dc9d8a86b2bdabe686f5e
Divyanshu Tiwari <91055535+divyanshu1300@users.noreply.github.com>
2023-01-11 21:21:55 +0530

    Add files via upload

---
+++ book_api/library/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from library.models import Book
+
+
+class BookSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields=['id','title','author','isbn','publisher']
+
+
+
+
